c0ce3f6aed2ffeca1b979b051331956fdeffee24
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2023-04-21 09:00:02 +0200

    inicial python flask

---
+++ src/weather.py
@@ -0,0 +1,3 @@
+def get_weather():
+    return {"id": "BIO", "name": "Bilbao", "temperature": 11, "rain_probability": 0.9}
+
---
+++ src/webserver.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+from .weather import get_weather
+
+app = Flask(__name__)
+
+@app.route("/cities/BIO")
+def hello_world():
+    return get_weather()

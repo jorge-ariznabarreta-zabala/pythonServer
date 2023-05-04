01f5f5445c5f0daca84ded814e3a5739ad37988c
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2023-05-04 13:21:03 +0200

    Metodos GET y POST funcionan

--- src/weather.py
+++ src/weather.py
@@ -1,3 +1,23 @@
-def get_weather():
-    return {"id": "BIO", "name": "Bilbao", "temperature": 11, "rain_probability": 0.9}
+WEATHER_DB={
+    'BIO':{
+        "id": "BIO",
+        "name": "Bilbao",
+        "temperature": 30,
+        "rain_probability": 0.5
+    },
+    'RMA':{
+        "id": "RMA",
+        "name": "Roma",
+        "temperature": 25,
+        "rain_probability": 0.3
+    },
+}
+def get_city_by(city_id):
+    return WEATHER_DB.get(city_id)
 
+def get_all_cities():
+    return WEATHER_DB
+
+def post_city(new_city):
+ 
+     WEATHER_DB[new_city['id']] = new_city
--- src/webserver.py
+++ src/webserver.py
@@ -1,11 +1,23 @@
-from flask import Flask
-from .weather import get_weather
+from flask import Flask, request
+from .weather import get_city_by, get_all_cities, post_city
+
 
 app = Flask(__name__)
 
-@app.route("/cities/BIO")
-def hello_world():
-    return get_weather()
+@app.route("/cities/<city_id>", methods=['GET'])
+def get_city(city_id):
+    return get_city_by(city_id)
+
+@app.route("/cities", methods=['GET'])
+def get_cities():
+    return get_all_cities()
+
+@app.route("/cities", methods=["POST"])
+def new_city():
+    data= request.get_json()
+    print ('**newcity', data['id'])
+    post_city(data)
+    return ""
 
 @app.route("/")
 def hello_root():
